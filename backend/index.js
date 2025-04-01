14bcc36fb88bb95d6b17a2c8042d27c05c8a24dd
Kushal Vala <kushalvala@mac.home>
2025-04-01 00:19:32 -0400

    minor changes with paget logo and title and minor changes in backend

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./src/lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +20,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// required this condition to give the path on the server
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(port, () => {
   console.log("server is runing on port 5001");
   connectDB();
--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -6,7 +6,6 @@ const ChatHeader = () => {
   const { selectedUser, setSelectedUser } = useChatStore();
   const { onlineUsers } = useAuthStore();
 
-  console.log(onlineUsers.includes(selectedUser._id));
   return (
     <div className="p-2.5 border-b border-base-300">
       <div className="flex items-center justify-between">
